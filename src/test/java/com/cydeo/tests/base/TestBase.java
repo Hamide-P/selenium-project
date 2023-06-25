e565521ddf5fd740ac8c6fc73dae834125b89877
mamihami <hamide.pehlivan@hotmail.com>
2023-06-25 23:22:29 +0200

    Review week4 changes added

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -18,17 +18,36 @@ public abstract class TestBase {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
+    public void setup(){
         // 1. Open Chrome browser
+        // Driver class will handle driver setting up line of codes
+        // Since we are using Driver class, we will not need to use setting up browser drivers codes
         driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+
+        // After we implement Driver class singleton design pattern in our project,
+        // we will not need above codes instead we will determine common line of codes from most test
+        // methods to implement in this class
+
+        // We can use this class for navigating to the page
+        // Driver.getDriver.get("URL of app");
+
+
+
     }
 
     @AfterMethod
-    public void tearDownMethod() {
+    public void tearDownMethod(){
         driver.quit();
         //driver.close();
+
+
+
+        // Driver.closeDriver();
+
+        // Taking screenshot codes here to get screenshot of failing Test after they done
+
     }
 }
